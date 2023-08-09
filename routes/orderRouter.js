5fb534c9084dba1e244b2abec68d132c2c3425e8
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-08-09 14:07:44 -0600

    - order by status route

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -5,6 +5,7 @@ const orderRouter = Router();
 
 orderRouter.get("/", orderController.getAll);
 orderRouter.get("/order/:id", orderController.getOne);
+orderRouter.get("/status", orderController.getByStatus);
 orderRouter.get("/total-sales", orderController.sales);
 orderRouter.post("/", orderController.create);
 orderRouter.put("/:id", orderController.update);
