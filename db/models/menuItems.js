21ebf26f26f4dea2d9d0613bdf1b4f59b15c7e5d
Randy J <randy.j416@yahoo.com>
2023-11-22 22:20:37 +0000

    add logic for deleting, not fully functional

--- db/models/menuItems.js
+++ db/models/menuItems.js
@@ -60,4 +60,9 @@ const update = async (id, body) => {
   return menuItem;
 };
 
-module.exports = { getAll, getOne, create, update, MenuItems };
+const remove = async (id) => {
+  const menuItem = await MenuItems.findByIdAndDelete(id);
+  return menuItem.id;
+};
+
+module.exports = { getAll, getOne, create, update, remove, MenuItems };
